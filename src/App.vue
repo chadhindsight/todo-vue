f95ef7b3c606d138f03dd071f1c4aaf8ee97ff5d
Chad Hinds <chadrickj8@gmail.com>
2020-08-20 18:05:56 -0400

    Add deleteTodo method REST logic

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,52 @@
 <template>
-  <Todos v-bind:todos="todos" />
+  <Header />
+  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  <AddTodo v-on:add-todo="addTodo" />
 </template>
 
 <script>
+import axios from "axios";
 import Todos from "./components/Todos";
+import Header from "./components/layouts/Header";
+import AddTodo from "./components/AddTodo.vue";
 
 export default {
   name: "App",
   components: {
     Todos,
+    Header,
+    AddTodo,
   },
   // A function that returns an object
   data() {
     return {
-      todos: [
-        {
-          id: 2,
-          title: "Make dinner",
-          completed: false,
-        },
-        {
-          id: 34,
-          title: "Buy groceries",
-          completed: false,
-        },
-        {
-          id: 21,
-          title: "Pay Cable bill",
-          completed: false,
-        },
-      ],
+      todos: [],
     };
+  },
+  methods: {
+    async deleteTodo(id) {
+      try {
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+        this.todos = this.todos.filter((todo) => todo.id !== id);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    },
+  },
+  // works similar to componentDidMount
+  async created() {
+    try {
+      let res = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos?_limit=7"
+      );
+      this.todos = res.data;
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
 </script>
